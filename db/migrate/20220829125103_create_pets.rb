b577891a574a44d8c1939f5cccf87feef7378737
yusufnviiri <yusufnviiri@gmail.com>
2022-08-29 16:05:07 +0300

    Add app files

---
+++ db/migrate/20220829125103_create_pets.rb
@@ -0,0 +1,11 @@
+class CreatePets < ActiveRecord::Migration[7.0]
+  def change
+    create_table :pets do |t|
+      t.string :name
+      t.text :owner
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
